a11a93c14a06e4230823b578c62888894e005f91
Toprak ÇAVUŞDAĞ <75522385+toprak-cavusdag@users.noreply.github.com>
2023-05-03 13:55:19 +0300

    Featured done

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import man from '../../asset/man.png';
+import searchIcon from '../../asset/search.png';
 import './Featured.scss';
 
 const Featured = () => {
@@ -6,23 +8,27 @@ const Featured = () => {
     <div className='featured'>
       <div className='container'>
         <div className='left'>
-          <h1>Find the perfect freelance services for your business</h1>
+          <h1>
+            Find the perfect <i>freelance</i> services for your business
+          </h1>
           <div className='search'>
             <div className='searchInput'>
-              <img src='' alt='search icon' />
+              <img src={searchIcon} alt='search icon' />
               <input type='text' placeholder='Try "building mobile app" ' />
             </div>
             <button>Search</button>
           </div>
-          <div className='popular'>
+          <div className='populer'>
             <span>Popular: </span>
-            <button>Web design</button>
-            <button>Wordpress</button>
-            <button>Logo Design</button>
-            <button>AI Services</button>
+            <button className='buttonCat'>Web design</button>
+            <button className='buttonCat'>Wordpress</button>
+            <button className='buttonCat'>Logo Design</button>
+            <button className='buttonCat'>AI Services</button>
           </div>
         </div>
-        <div className='right'></div>
+        <div className='right'>
+          <img src={man} alt='man image' />
+        </div>
       </div>
     </div>
   );
